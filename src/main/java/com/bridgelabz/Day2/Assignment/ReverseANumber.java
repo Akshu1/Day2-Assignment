3514cec540e6fe3bc005b6fec7b8d66c79dc039a
akshu1 <akshathanagaraj98@gmail.com>
2022-11-28 15:17:51 +0530

    Add:Programs with Loops,Switch

---
+++ src/main/java/com/bridgelabz/Day2/Assignment/ReverseANumber.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.Day2.Assignment;
+
+public class ReverseANumber {
+
+	public static void main(String[] args) {
+
+		int num = 1234567, reversed = 0;
+
+		for (; num != 0; num /= 10) {
+			int digit = num % 10;
+			reversed = reversed * 10 + digit;
+		}
+
+		System.out.println("Reversed Number: " + reversed);
+	}
+}
